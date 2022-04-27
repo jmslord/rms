acfca356d6d1a4b5844b75c2c375918c926a3d8e
jmslord <jmseffendy@gmail.com>
2022-04-27 13:44:13 +0700

    First Change

--- src/main/java/com/untarsoftdev8/rms/Home.java
+++ src/main/java/com/untarsoftdev8/rms/Home.java
@@ -12,7 +12,7 @@ package com.untarsoftdev8.rms;
 public class Home extends javax.swing.JFrame {
 
     /**
-     * Creates new form JavaPOS
+     * Creates new form Home
      */
     public Home() {
         initComponents();
